b14534d83472fa4805a2a5de9210b99412fae034
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-07-24 09:37:37 +0000

    remove creating access key on setup

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,9 +8,6 @@ CERT_OPENSSL_FINGERPRINT="$(openssl x509 -in "${SB_CERTIFICATE_FILE}" -noout -sh
 CERT_HEX_FINGERPRINT="$(echo "${CERT_OPENSSL_FINGERPRINT#*=}" | tr -d :)" || return
 
 PUBLIC_API_URL="https://${PUBLIC_HOSTNAME}:8081/${SB_API_PREFIX}"
-LOCAL_API_URL="https://localhost:8081/${SB_API_PREFIX}"
-
-curl --silent --show-error --fail --insecure --request POST "${LOCAL_API_URL}/access-keys" >&2
 
 jq -n --arg hostname "${PUBLIC_HOSTNAME}" '{"portForNewAccessKeys":8082, "hostname":$hostname}' > "${SB_STATE_DIR}/shadowbox_server_config.json"
 
